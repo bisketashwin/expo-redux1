15a5ca00a9a6e6ec16ea42f160f33480a2b19a64
bisketashwin <bisketashwin@gmail.com>
2023-09-07 12:34:53 +0530

    WIP - Color Name and match
    ColorNameConverter implemented in JSX
    
    now taking the information color, name and close match info need to pass to the actions and store

--- store/colorSlice.js
+++ store/colorSlice.js
@@ -2,29 +2,54 @@
 ////https://redux-toolkit.js.org/usage/usage-with-typescript#createslice
 
 import { createSlice } from "@reduxjs/toolkit";
+import ColorNameConverter from "../plugIns/ColorNameConverter";
 
 const initialState = {
   value: [],
 };
+
+
+function rgbToHex(red, green, blue) {
+  // Convert each component to a hexadecimal string and pad with zeros if needed
+  const redHex = red.toString(16).padStart(2, '0');
+  const greenHex = green.toString(16).padStart(2, '0');
+  const blueHex = blue.toString(16).padStart(2, '0');
+  
+  // Combine the components to form the hex color code
+  return `#${redHex}${greenHex}${blueHex}`;
+}
 
 const randomRgb = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
 
-  return `rgb(${red}, ${green}, ${blue})`;
+  const hexColor = rgbToHex(red,green,blue)
+  const color = `rgb(${red}, ${green}, ${blue})`
+  return ({color,hexColor});
 };
+
+const generateColorName = ()=>{
+  const inputColor = randomRgb()
+  const {rgbColor,hexColor} = inputColor
+  const colorName = ColorNameConverter(hexColor)
+  console.log(colorName)
+
+  return(
+    color
+  )
+}
 
 export const colorSlice = createSlice({
   name: "color",
   initialState,
   reducers: {
     setColor: (state) => {
-      state.value = [...state.value, randomRgb()];
+      state.value = [...state.value, generateColorName()];
     },
     editColor: (state, action) => {
-      const [ oldColor, newColor ] = action.payload;
-      console.log( action.payload);
+      const [oldColor, newColor] = action.payload;
+      console.log(action.payload);
       console.log(oldColor, newColor);
       const index = state.value.indexOf(oldColor);
       if (index !== -1) {
@@ -33,23 +58,13 @@ export const colorSlice = createSlice({
     },
     deleteColor: (state, action) => {
       const colorToDelete = action.payload;
-      
+
       state.value = state.value.filter((color) => color !== colorToDelete);
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {  setColor, editColor, deleteColor } = colorSlice.actions;
+export const { setColor, editColor, deleteColor } = colorSlice.actions;
 
 export default colorSlice.reducer;
-
-
-// case DELETE_USER:
-//       // Implement logic to delete a user from the state by their ID
-//       const updatedUsersDelete = state.users.filter((user) => user.id !== action.payload.id);
-//       return { ...state, users: updatedUsersDelete };
-//     case EDIT_USER:
-//       // Implement logic to edit a user in the state by their ID
-//       const updatedUsersEdit = state.users.map((user) =>
-//         user.id === action.payload.oldUser.id ? action.payload.newUser : user
